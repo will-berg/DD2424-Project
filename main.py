fd823511be8ad287f4abbe9de61b549910534e94
will-berg <wm.bg@outlook.com>
2023-05-17 00:49:04 +0200

    try on linux

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ transform = transforms.Compose([
     transforms.RandomCrop([300, 300], 1, pad_if_needed=True),
     transforms.ToTensor()
 ])
+
 oxford_dataset = datasets.OxfordIIITPet("./datasets/oxfordIIITPet/", download=True)#, transform=transform, )
-oxford_dataset = torch.utils.data.Subset(oxford_dataset)
+# oxford_dataset = torch.utils.data.Subset(oxford_dataset)
 
 #i = 0
 #for img, cl in oxford_dataset:
@@ -35,39 +36,75 @@ oxford_dataset = torch.utils.data.Subset(oxford_dataset)
 #    breed_to_dog_cat[cat_label] = 1
 
 # Filter dataset for dogs and cats
-dataloader = torch.utils.data.DataLoader(oxford_dataset, batch_size=4, shuffle=True)
-
+size = len(oxford_dataset)
 
 model = models.resnet18(pretrained=True)
 model.fc = nn.Linear(512, 37)
 
 
-# Training
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-loss_fn = nn.CrossEntropyLoss()
+# Training and testing
+training_set, testing_set = torch.utils.data.random_split(oxford_dataset, [int(0.85 * size), int(0.15 * size)])
 
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = "cpu"
-model.to(device)
+def train():
+	model.train()
 
-n_epochs = 10
-for epoch in range(n_epochs):
-    model.train()
-    total_loss = 0
-    for images, labels in dataloader:
-        images = images.to(device)
-        labels = labels.to(device)
+	learning_rate = 0.001
+	n_epochs = 10
 
-        # Forward pass
-        outputs = model(images)
-        loss = loss_fn(outputs, labels)
+	optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+	loss_function = nn.CrossEntropyLoss()
 
-        # Backward pass and optimization
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	model.to(device)
 
-        total_loss += loss.item()
-    # Print the average loss for the epoch
-    epoch_loss = total_loss / len(dataloader)
-    print(f"Epoch {epoch+1}/{n_epochs}, Loss: {epoch_loss:.4f}")
+	training_loader = torch.utils.data.DataLoader(training_set, batch_size=4, shuffle=True)
+
+	for epoch in range(n_epochs):
+		total_loss = 0
+		for images, labels in training_loader:
+			images = images.to(device)
+			labels = labels.to(device)
+
+			# Forward pass
+			outputs = model(images)
+			loss = loss_function(outputs, labels)
+
+			# Backward pass and optimization
+			optimizer.zero_grad()
+			loss.backward()
+			optimizer.step()
+
+			total_loss += loss.item()
+
+		# Print the average loss for the epoch
+		epoch_loss = total_loss / len(training_loader)
+		print(f"Epoch {epoch+1}/{n_epochs}, Loss: {epoch_loss:.4f}")
+
+def test():
+	model.eval()
+
+	correct = 0
+	testing_loader = torch.utils.data.DataLoader(testing_loader, batch_size=4, shuffle=True)
+
+	with torch.no_grad():
+		for images, labels in testing_loader:
+			# Forward pass, returns model output
+			probabilities = model(images)
+			_, predictions = torch.max(probabilities, 1)
+			correct += torch.sum(labels == predictions)
+
+	accuracy = correct / len(testing_set)
+	return accuracy
+
+
+""" probabilities = torch.tensor([[0, 0.2, 0.3, 0.5], [0, 0.2, 0.3, 0.5]])
+labels = torch.tensor([3, 2])
+
+_, predictions = torch.max(probabilities, 1)
+correct = torch.sum(labels == predictions)
+print(predictions)
+
+print(correct)
+# same = torch.sum(list1 == list2)
+# print(same / len(list1))
+ """
