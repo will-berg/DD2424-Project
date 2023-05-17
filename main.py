e69fb3ca3e50f61fec905fc48f4fbbd5003c2009
will-berg <wm.bg@outlook.com>
2023-05-17 17:48:43 +0200

    training and testing

--- main.py
+++ main.py
@@ -2,17 +2,9 @@ import torch
 import torch.nn as nn
 from torchvision import datasets, models, transforms
 import torch.optim as optim
-import numpy as np
-import pandas as pd
 
 # Task 1
-transform = transforms.Compose([
-    transforms.RandomCrop([300, 300], 1, pad_if_needed=True),
-    transforms.ToTensor()
-])
-
-oxford_dataset = datasets.OxfordIIITPet("./datasets/oxfordIIITPet/", download=True)#, transform=transform, )
-# oxford_dataset = torch.utils.data.Subset(oxford_dataset)
+#oxford_dataset = torch.utils.data.Subset(oxford_dataset)
 
 #i = 0
 #for img, cl in oxford_dataset:
@@ -35,29 +27,19 @@ oxford_dataset = datasets.OxfordIIITPet("./datasets/oxfordIIITPet/", download=Tr
 #for cat_label in cats_labels:
 #    breed_to_dog_cat[cat_label] = 1
 
-# Filter dataset for dogs and cats
-size = len(oxford_dataset)
+# Parameters
+learning_rate = 0.001
+n_epochs = 25
+batch_size = 16
+loss_function = nn.CrossEntropyLoss()
+model_name = f"{n_epochs}-{batch_size}"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = models.resnet18(pretrained=True)
-model.fc = nn.Linear(512, 37)
-
-
-# Training and testing
-training_set, testing_set = torch.utils.data.random_split(oxford_dataset, [int(0.85 * size), int(0.15 * size)])
-
-def train():
+def train(model, training_set, optimizer):
 	model.train()
-
-	learning_rate = 0.001
-	n_epochs = 10
-
-	optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-	loss_function = nn.CrossEntropyLoss()
-
-	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 	model.to(device)
 
-	training_loader = torch.utils.data.DataLoader(training_set, batch_size=4, shuffle=True)
+	training_loader = torch.utils.data.DataLoader(training_set, batch_size=batch_size, shuffle=True)
 
 	for epoch in range(n_epochs):
 		total_loss = 0
@@ -80,31 +62,81 @@ def train():
 		epoch_loss = total_loss / len(training_loader)
 		print(f"Epoch {epoch+1}/{n_epochs}, Loss: {epoch_loss:.4f}")
 
-def test():
+	# Save the trained model
+	torch.save(model.state_dict(), f"models/{model_name}.pth")
+
+def test(model, testing_set):
+	# Load the trained model
+	model.load_state_dict(torch.load(f"models/{model_name}.pth"))
+	model.to(device)
 	model.eval()
 
 	correct = 0
-	testing_loader = torch.utils.data.DataLoader(testing_loader, batch_size=4, shuffle=True)
+	testing_loader = torch.utils.data.DataLoader(testing_set, batch_size=batch_size, shuffle=True)
 
 	with torch.no_grad():
 		for images, labels in testing_loader:
+			images = images.to(device)
+			labels = labels.to(device)
 			# Forward pass, returns model output
 			probabilities = model(images)
 			_, predictions = torch.max(probabilities, 1)
 			correct += torch.sum(labels == predictions)
 
-	accuracy = correct / len(testing_set)
-	return accuracy
+	acc = correct.item() / len(testing_set)
+	print(f"Testing accuracy: {acc:.4f}")
+
+def training_accuracy(model, training_set):
+	# Load the trained model
+	model.load_state_dict(torch.load(f"models/{model_name}.pth"))
+	model.to(device)
+	model.eval()
+
+	correct = 0
+	training_loader = torch.utils.data.DataLoader(training_set, batch_size=batch_size, shuffle=True)
+
+	with torch.no_grad():
+		for images, labels in training_loader:
+			images = images.to(device)
+			labels = labels.to(device)
+			# Forward pass, returns model output
+			probabilities = model(images)
+			_, predictions = torch.max(probabilities, 1)
+			correct += torch.sum(labels == predictions)
+
+	acc = correct.item() / len(training_set)
+	print(f"Training accuracy: {acc:.4f}")
+
+if __name__ == "__main__":
+	transform = transforms.Compose([
+		transforms.RandomCrop([300, 300], 1, pad_if_needed=True),
+		transforms.ToTensor()
+	])
+	oxford_dataset = datasets.OxfordIIITPet("./datasets/oxfordIIITPet/", download=True, transform=transform)
+
+	model = models.resnet18(weights="ResNet18_Weights.DEFAULT")
+	model.fc = nn.Linear(512, 37)
+
+	optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+	# Training and testing
+	training_set, testing_set = torch.utils.data.random_split(oxford_dataset, [int(0.90 * len(oxford_dataset)), int(0.10 * len(oxford_dataset))])
+	# train(model, training_set, optimizer)
+	# test(model, testing_set)
+	training_accuracy(model, training_set)
 
 
-""" probabilities = torch.tensor([[0, 0.2, 0.3, 0.5], [0, 0.2, 0.3, 0.5]])
-labels = torch.tensor([3, 2])
 
-_, predictions = torch.max(probabilities, 1)
-correct = torch.sum(labels == predictions)
-print(predictions)
 
-print(correct)
+
+
+
+# probabilities = torch.tensor([[0, 0.2, 0.3, 0.5], [0, 0.2, 0.3, 0.5]])
+# labels = torch.tensor([3, 2])
+
+# _, predictions = torch.max(probabilities, 1)
+# correct = torch.sum(labels == predictions)
+
+# print(correct.item())
 # same = torch.sum(list1 == list2)
 # print(same / len(list1))
- """
