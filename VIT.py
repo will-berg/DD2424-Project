a0b405962fd82e8f17d39991b5fbc2619a5d4e01
Anton Forsman <10177340+anforsm@users.noreply.github.com>
2023-05-18 13:32:59 +0200

    VIT for oxford pets

--- VIT.py
+++ VIT.py
@@ -1,41 +1,34 @@
 import random
 from PIL import ImageDraw, ImageFont, Image
-from datasets import load_dataset
+from datasets import load_dataset, load_metric
+from transformers import ViTFeatureExtractor, ViTForImageClassification, Trainer, TrainingArguments
+import torch
+import numpy as np
 
-ds = load_dataset("beans")
 
-def show_example(ds, seed = 1234, examples_per_class = 3, size=(350, 350)):
-    w, h = size
-    labels = ds["train"].features["labels"].names
-    grid = Image.new("RGB", size=(examples_per_class * w, len(labels) * h))
-    draw = ImageDraw.Draw(grid)
-    font = ImageFont.truetype("arial.ttf", size=20)
 
-    for label_id, label in enumerate(labels):
-        ds_slice = ds["train"].filter(lambda example: example["labels"] == label_id).shuffle(seed).select(range(examples_per_class))
-
-        for i, example in enumerate(ds_slice):
-            image = example["image"]
-            idx = examples_per_class * label_id + i
-            box = (idx % examples_per_class * w, idx // examples_per_class * h)
-            grid.paste(image.resize(size), box=box)
-            draw.text(box, label, (255, 255, 255), font=font)
-
-    return grid
-
-from transformers import ViTFeatureExtractor
+ds = load_dataset("pcuenq/oxford-pets")
+ds = ds["train"].train_test_split(test_size=0.1, seed=42)
 
 model_name = "google/vit-base-patch16-224-in21k"
 feature_extractor = ViTFeatureExtractor.from_pretrained(model_name)
 
-def transfrom(example_batch):
-    inputs = feature_extractor([x for x in example_batch["image"]], return_tensors="pt")
-    inputs["labels"] = example_batch["labels"]
-    return inputs
-  
-prepared_ds = ds.with_transform(transfrom)
+labels = []
+for example in ds["train"]:
+    if example["label"] not in labels:
 
-import torch
+        labels.append(example["label"])
+
+
+ds_labeltoid = {label: i for i, label in enumerate(labels)}
+ds_idtolabel = {i: label for i, label in enumerate(labels)}
+
+def transform(example_batch):
+    inputs = feature_extractor([x.convert("RGB") for x in example_batch["image"]], return_tensors="pt")
+    inputs["labels"] = [ds_labeltoid[y] for y in example_batch["label"]]
+    return inputs
+
+ds_p = ds.with_transform(transform)
 
 def collate_fn(batch):
     return {
@@ -43,18 +36,10 @@ def collate_fn(batch):
         "labels": torch.tensor([example["labels"] for example in batch]),
     }
 
-import numpy as np
-from datasets import load_metric
-
 metric = load_metric("accuracy")
+
 def compute_metrics(p):
-    pred, labels = p
-    pred = np.argmax(pred, axis=1)
-    return metric.compute(predictions=pred, references=labels)
-
-from transformers import ViTForImageClassification
-
-labels = ds["train"].features["labels"].names
+    return metric.compute(predictions=np.argmax(p.predictions, axis=1), references=p.label_ids)
 
 model = ViTForImageClassification.from_pretrained(
     model_name, 
@@ -63,10 +48,8 @@ model = ViTForImageClassification.from_pretrained(
     label2id={c: i for i, c in enumerate(labels)},
 )
 
-from transformers import TrainingArguments
-
 training_args = TrainingArguments(
-    output_dir="./vit-base-beans",
+    output_dir="./vit-base-oxford",
     per_device_train_batch_size=16,
     evaluation_strategy="steps",
     num_train_epochs=4,
@@ -88,8 +71,8 @@ trainer = Trainer(
     args=training_args,
     data_collator=collate_fn,
     compute_metrics=compute_metrics,
-    train_dataset=prepared_ds["train"],
-    eval_dataset=prepared_ds["validation"],
+    train_dataset=ds_p["train"],
+    eval_dataset=ds_p["test"],
     tokenizer=feature_extractor,
 )
 
@@ -100,6 +83,7 @@ trainer.save_metrics("train", train_results.metrics)
 trainer.save_state()
 
 
-metrics = trainer.evaluate(prepared_ds["validation"])
+metrics = trainer.evaluate(ds_p["test"])
 trainer.log_metrics("eval", metrics)
 trainer.save_metrics("eval", metrics)
+
